0890296f3b5757b921f359818e4503556ccdf6ee
Lisina <lisina.pro@gmail.com>
2025-09-19 20:45:31 +0300

    ServiceCalc Budget Reduction

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -5,14 +5,10 @@ public class Main {
         ServiceCalc service = new ServiceCalc();
         int income = 100_000;
         int expenses = 60_000;
-        int threshold = 150_000;
-        int vacation = ((income - expenses) / 3);
-        //boolean b = vacation > threshold;
-        {
+        int threshold = 150_000; {
 
         }
         int count = service.calculate(income, expenses, threshold);
-            System.out.println(count);
-            System.out.println(vacation);
-        }
+        System.out.println(count);
     }
+}
--- src/main/java/ru/netology/services/ServiceCalc.java
+++ src/main/java/ru/netology/services/ServiceCalc.java
@@ -1,16 +1,16 @@
 package ru.netology.services;
-
 public class ServiceCalc {
     public int calculate(int income, int expenses, int threshold) {
-        int count = 12; // счётчик месяцев отдыха
-        //int threshold = 20000; // количество денег на счету
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
         for (int month = 0; month < 12; month++) {
-            if (threshold >= expenses) { // можем ли отдыхать?
+            if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                threshold = threshold - expenses;
+                money = income - expenses;
+                money = money / 3;
             } else {
-                threshold = threshold + income;
-
+                money += income;
+                money-=expenses;
             }
         }
         return count;
